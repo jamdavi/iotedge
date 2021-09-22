d6d954c1d72aded533c60a769ac5ab1ccb32d474
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-09-22 09:39:06 -0700

    Add support for DPS-provided trust bundles

--- edgelet/edgelet-http-workload/src/lib.rs
+++ edgelet/edgelet-http-workload/src/lib.rs
@@ -121,6 +121,7 @@ where
             device_id: "test-device".to_string(),
             trust_bundle: "test-trust-bundle".to_string(),
             manifest_trust_bundle: "test-manifest-trust-bundle".to_string(),
+            dps_trust_bundle: "test-dps-trust-bundle".to_string(),
             edge_ca_cert: "test-ca-cert".to_string(),
             edge_ca_key: "test-ca-key".to_string(),
         };
@@ -166,6 +167,7 @@ struct WorkloadConfig {
 
     trust_bundle: String,
     manifest_trust_bundle: String,
+    dps_trust_bundle: String,
 
     edge_ca_cert: String,
     edge_ca_key: String,
@@ -201,6 +203,7 @@ impl WorkloadConfig {
 
             trust_bundle,
             manifest_trust_bundle,
+            dps_trust_bundle: settings.dps_trust_bundle().to_string(),
 
             edge_ca_cert,
             edge_ca_key,
@@ -232,6 +235,7 @@ mod tests {
 
                 trust_bundle: edgelet_settings::TRUST_BUNDLE_ALIAS.to_string(),
                 manifest_trust_bundle: edgelet_settings::MANIFEST_TRUST_BUNDLE_ALIAS.to_string(),
+                dps_trust_bundle: settings.dps_trust_bundle,
 
                 edge_ca_cert: edgelet_settings::AZIOT_EDGED_CA_ALIAS.to_string(),
                 edge_ca_key: edgelet_settings::AZIOT_EDGED_CA_ALIAS.to_string(),
@@ -256,6 +260,7 @@ mod tests {
             edge_ca_key: Some("test-ca-key".to_string()),
             trust_bundle: Some("test-trust-bundle".to_string()),
             manifest_trust_bundle: Some("test-manifest-trust-bundle".to_string()),
+            dps_trust_bundle: "test-dps-trust-bundle".to_string(),
         };
 
         // Check that values from settings are used when provided.
@@ -267,6 +272,7 @@ mod tests {
 
                 trust_bundle: "test-trust-bundle".to_string(),
                 manifest_trust_bundle: "test-manifest-trust-bundle".to_string(),
+                dps_trust_bundle: "test-dps-trust-bundle".to_string(),
 
                 edge_ca_cert: "test-ca-cert".to_string(),
                 edge_ca_key: "test-ca-key".to_string(),
